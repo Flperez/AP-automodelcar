8a8b164d7a1e1454be781d7edebfeee7d14e753a
Flperez <felipeperez2710@gmail.com>
2017-05-16 13:34:16 +0200

    Add files via upload

---
+++ catkin_ws_user/src/master/src/master.cpp
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <vector>
+#include <iostream>
+#include "ros/ros.h"
+#include "std_msgs/MultiArrayLayout.h"
+#include "std_msgs/MultiArrayDimension.h"
+#include "std_msgs/Int32MultiArray.h"
+
+using namespace std;
+
+int Arr[6];
+void arrayCallback(const std_msgs::Int32MultiArray::ConstPtr& array);
+
+int main(int argc, char **argv)
+{
+
+	ros::init(argc, argv, "arraySubscriber");
+
+	ros::NodeHandle n;	
+
+	ros::Subscriber sub3 = n.subscribe("/data", 100, arrayCallback);
+
+	
+
+	for(int j = 0; j < 6; j++)
+	{
+		printf("%d ", Arr[j]);
+	}
+
+	printf("\n");
+    ros::spin();
+	return 0;
+}
+
+void arrayCallback(const std_msgs::Int32MultiArray::ConstPtr& array)
+{
+
+	int i = 0;
+	// print all the remaining numbers
+	for(std::vector<int>::const_iterator it = array->data.begin(); it != array->data.end(); ++it)
+	{
+		Arr[i] = *it;
+		i++;
+	}
+
+	return;
+}
